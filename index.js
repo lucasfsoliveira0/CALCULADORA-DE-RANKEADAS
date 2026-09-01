f42a7503ac244153040f9ed39b2e0346c91f1bd5
lucasfsoliveira0 <lucas.f.s.oliveiraads@gmail.com>
2026-09-01 15:32:33 -0300

    SegundoContato

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 //CALCULADORA DE RANKEADAS 
+let numeroVitorias = (25)
+let numeroDerrotas = (5)
+let saldo = (numeroVitorias - numeroDerrotas)
+
+console.log(saldo)
+
+function calcularRank(saldo) {
+    if (saldo >= 20) {
+        console.log("Você está no rank Diamante")
+    } else if (saldo >= 15) {
+        console.log("Você está no rank Ouro")
+    } else if (saldo >= 10) {
+        console.log("Você está no rank Prata")
+    } else {
+        console.log("Você está no rank Bronze")
+    }
+}
+calcularRank(saldo)
