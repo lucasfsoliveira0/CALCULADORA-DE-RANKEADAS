deba1f2509dc920ebd7a6c5eb6996115cda34986
lucasfsoliveira0 <lucas.f.s.oliveiraads@gmail.com>
2026-09-01 15:36:26 -0300

    terceirocontato

--- index.js
+++ index.js
@@ -7,13 +7,13 @@ console.log(saldo)
 
 function calcularRank(saldo) {
     if (saldo >= 20) {
-        console.log("Você está no rank Diamante")
+        console.log("O Heroi tem saldo de" + saldo +"vitorias" "e está no rank Diamante")
     } else if (saldo >= 15) {
-        console.log("Você está no rank Ouro")
+        console.log("O Heroi tem saldo de" + saldo +"vitorias" "e está no rank Ouro")
     } else if (saldo >= 10) {
-        console.log("Você está no rank Prata")
+        console.log("O Heroi tem saldo de" + saldo +"vitorias" "e está no rank Prata")
     } else {
-        console.log("Você está no rank Bronze")
+        console.log("O Heroi tem saldo de" + saldo +"vitorias" "e está no rank Bronze")
     }
 }
 calcularRank(saldo)
